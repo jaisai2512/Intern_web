6cd01f979dd2f228e6c41433d572626ba435a851
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 22:01:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,31 +3,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.sidebar.header('Input here')
-user_input = st.sidebar.text_input("Enter Register No:")
 
-df=pd.read_excel('intern21.xlsx')
-st.write(df)
-
-st.markdown('### Report')
-col1, col2= st.columns(2)
-col1.metric("Name", df['Name'][0])
-col2.metric("Reg_No", df['Reg_no'][0])
-
-col3, col4= st.columns(2)
-col3.metric('Percentage', abs(weight-100))
-col4.metric("Gender", df['Gender'][0])
-
-required_skills = {"Cloud Computing":{
-    "Cloud_Platforms":{"AWS":1,"AZURE": 2,"GCP": 1},
-    "Networking fundamentals":{"TCP/IP":1,"DNS": 2,"Load balancing":5,"VPN":5 },
-    "OS":{"Linux":4, "Windows":5}, 
-    "Automation & Scripting":{"Python":2,"Powershell":3,"Bash":4},
-    "IAC":{"Terraform":1 ,"AWS cloud formation":2},
-    "Containerization":{"Docker":1,"Kubernetes":2},
-    "Monitoring & Troubleshooting":{"AWS cloudwatch":1,"Azure monitor":2},
-    "Softskills":{"Effective communication":1,"Teamwork":1,"Collaboration skills":1}
-}}
 
 def gap_analysis(Aspiration:str,data,required_skills):
     x=[]
@@ -54,6 +30,33 @@ def known(data,required_skills):
 
 weight,unknown=gap_analysis('data',df,required_skills)
 weight=abs((weight-100))/10
+
+st.sidebar.header('Input here')
+user_input = st.sidebar.text_input("Enter Register No:")
+
+df=pd.read_excel('intern21.xlsx')
+st.write(df)
+
+st.markdown('### Report')
+col1, col2= st.columns(2)
+col1.metric("Name", df['Name'][0])
+col2.metric("Reg_No", df['Reg_no'][0])
+
+col3, col4= st.columns(2)
+col3.metric('Percentage', abs(weight-100))
+col4.metric("Gender", df['Gender'][0])
+
+required_skills = {"Cloud Computing":{
+    "Cloud_Platforms":{"AWS":1,"AZURE": 2,"GCP": 1},
+    "Networking fundamentals":{"TCP/IP":1,"DNS": 2,"Load balancing":5,"VPN":5 },
+    "OS":{"Linux":4, "Windows":5}, 
+    "Automation & Scripting":{"Python":2,"Powershell":3,"Bash":4},
+    "IAC":{"Terraform":1 ,"AWS cloud formation":2},
+    "Containerization":{"Docker":1,"Kubernetes":2},
+    "Monitoring & Troubleshooting":{"AWS cloudwatch":1,"Azure monitor":2},
+    "Softskills":{"Effective communication":1,"Teamwork":1,"Collaboration skills":1}
+}}
+
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
