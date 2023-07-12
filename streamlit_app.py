77a9eb5fe0665a86d0b533218b85c877907da098
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 20:33:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+import plotly.graph_objects as go
+import pandas as pd
+import streamlit as st
+import plotly.express as px
 
+st.sidebar.header('Input here')
+user_input = st.sidebar.text_input("Enter Register No:")
