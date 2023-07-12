82417d11c3adb274172a7f5f8104620676339a5b
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 22:10:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ col1.metric("Name", df['Name'][0])
 col2.metric("Reg_No", df['Reg_no'][0])
 
 col3, col4= st.columns(2)
-col3.metric('Percentage',weight1)
+col3.metric('Percentage',abs(weight1-100))
 col4.metric("Gender", df['Gender'][0])
 
 
