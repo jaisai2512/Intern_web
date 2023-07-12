8a6e72b73b4286129b95236f26c30127d0604c3a
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 22:09:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,19 @@
+
 import plotly.graph_objects as go
 import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-
+required_skills = {"Cloud Computing":{
+    "Cloud_Platforms":{"AWS":1,"AZURE": 2,"GCP": 1},
+    "Networking fundamentals":{"TCP/IP":1,"DNS": 2,"Load balancing":5,"VPN":5 },
+    "OS":{"Linux":4, "Windows":5}, 
+    "Automation & Scripting":{"Python":2,"Powershell":3,"Bash":4},
+    "IAC":{"Terraform":1 ,"AWS cloud formation":2},
+    "Containerization":{"Docker":1,"Kubernetes":2},
+    "Monitoring & Troubleshooting":{"AWS cloudwatch":1,"Azure monitor":2},
+    "Softskills":{"Effective communication":1,"Teamwork":1,"Collaboration skills":1}
+}}
 
 
 
@@ -51,16 +61,7 @@ col3, col4= st.columns(2)
 col3.metric('Percentage',weight1)
 col4.metric("Gender", df['Gender'][0])
 
-required_skills = {"Cloud Computing":{
-    "Cloud_Platforms":{"AWS":1,"AZURE": 2,"GCP": 1},
-    "Networking fundamentals":{"TCP/IP":1,"DNS": 2,"Load balancing":5,"VPN":5 },
-    "OS":{"Linux":4, "Windows":5}, 
-    "Automation & Scripting":{"Python":2,"Powershell":3,"Bash":4},
-    "IAC":{"Terraform":1 ,"AWS cloud formation":2},
-    "Containerization":{"Docker":1,"Kubernetes":2},
-    "Monitoring & Troubleshooting":{"AWS cloudwatch":1,"Azure monitor":2},
-    "Softskills":{"Effective communication":1,"Teamwork":1,"Collaboration skills":1}
-}}
+
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
