c07ed40458f06c0c07804a41dc73ee7bbb08d8af
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 22:02:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,31 +5,7 @@ import plotly.express as px
 
 
 
-def gap_analysis(Aspiration:str,data,required_skills):
-    x=[]
-    p=0
-    known_skills=known(data,required_skills)
-    for i in required_skills[data['Aspiration'].to_list()[0]]:
-        for k,l in required_skills[data['Aspiration'].to_list()[0]][i].items():
-            if k in known_skills:
-                 print(k)
-            else:
-                x.append(k)
-                p+=required_skills[data['Aspiration'].to_list()[0]][i][k]
-    return p,x
 
-def known(data,required_skills):
-    o=[]
-    for i,j in required_skills[data['Aspiration'].to_list()[0]].items():
-        for k in data[i].to_list():
-                if(',' in k):
-                    o=o+k.split(',')
-                else:
-                    o.append(k)
-    return o
-
-weight,unknown=gap_analysis('data',df,required_skills)
-weight=abs((weight-100))/10
 
 st.sidebar.header('Input here')
 user_input = st.sidebar.text_input("Enter Register No:")
