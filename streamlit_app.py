157eea0e0bdf73da42b8a8e475f59a8a2ffedad4
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 21:08:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,41 @@ col3, col4= st.columns(2)
 col3.metric('DOB', str(df['DOB'][0]))
 col4.metric("Gender", df['Gender'][0])
 
-  
+required_skills = {"Cloud Computing":{
+    "Cloud_Platforms":{"AWS":1,"AZURE": 2,"GCP": 1},
+    "Networking fundamentals":{"TCP/IP":1,"DNS": 2,"Load balancing":5,"VPN":5 },
+    "OS":{"Linux":4, "Windows":5},
+    "Automation & Scripting":{"Python":2,"Powershell":3,"Bash":4},
+    "IAC":{"Terraform":1 ,"AWS cloud formation":2},
+    "Containerization":{"Docker":1,"Kubernetes":2},
+    "Monitoring & Troubleshooting":{"AWS cloudwatch":1,"Azure monitor":2},
+    "Softskills":{"Effective communication":1,"Teamwork":1,"Collaboration skills":1}
+}}
+
+def gap_analysis(Aspiration:str,data,required_skills,known_skills):
+    x=[]
+    p=0
+    known_skills=known(data,required_skills)
+    for i in required_skills[data['Aspiration'].to_list()[0]]:
+        for k,l in required_skills[data['Aspiration'].to_list()[0]][i].items():
+            if k in known_skills:
+                 print(k)
+            else:
+                x.append(k)
+                p+=required_skills[data['Aspiration'].to_list()[0]][i][k]
+    st.write(p)
+    st.write(x)
+
+def known(data,required_skills):
+    o=[]
+    for i,j in required_skills[data['Aspiration'].to_list()[0]].items():
+        for k in data[i].to_list():
+                if(',' in k):
+                    o=o+k.split(',')
+                else:
+                    o.append(k)
+    return o
+
+
+
+
