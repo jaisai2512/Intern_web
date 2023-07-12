9bed9e7fb2dbbf912ca28351848289bdaebd55a4
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 20:57:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,9 @@ st.markdown('### Report')
 col1, col2= st.columns(2)
 col1.metric("Name", df['Name'][0])
 col2.metric("Reg_No", df['Reg_no'][0])
+
+col3, col4= st.columns(2)
+col3.metric('DOB', df['DOB'][0])
+col4.metric("Gender", df['Gender'][0])
+
   
