0085fbd43ea1f90f2e1fc8052c2c17887a476a62
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-07-12 20:52:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ user_input = st.sidebar.text_input("Enter Register No:")
 
 df=pd.read_excel('intern21.xlsx')
 st.write(df)
+
+st.markdown('### Report')
+  col1, col2= st.columns(2)
+  col1.metric("Name", df['Name'])
+  col2.metric("Reg_No", df['Reg_no'])
+  
