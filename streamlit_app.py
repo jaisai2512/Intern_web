55f474f638cedada158df77a424094c87da9da59
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-08-17 20:21:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ if user_input :
 
     def known(data,required_skills):
         o=[]
-        for i,j in required_skills[data['Aspiration'].to_list()].items():
+        for i,j in required_skills[data['Aspiration'].to_list()[0]].items():
+            st.write(i)
             for k in data[i].to_list():
                     if(',' in k):
                         o=o+k.split(',')
